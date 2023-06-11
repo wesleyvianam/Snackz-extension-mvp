d8e5be3e12b1324001269c6c6bab5c0e2cfbb38d
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-06-11 13:31:53 -0300

    melhorias de layout

--- src/index.js
+++ src/index.js
@@ -42,31 +42,14 @@ meuLanche.addEventListener("submit", function(event) {
   };
 
   // Salva na API
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log("Resposta do servidor:", data);
-  })
-  .catch(error => {
-    console.error("Erro ao enviar solicitação:", error);
-  });
-
-  // fecha o popup
-  setTimeout(() => {
-    window.close();
-  }, 500);
+  fazerRequisicao(url, data);
 }); 
 
 // Alterna entre Pedir e Pedidos
 const pedido = document.querySelector('#pedidos');
 const pedir = document.querySelector('#pedir');
 const sectionPedidos = document.querySelector('#section-pedidos');
+const title = document.querySelector('#title');
 
 pedir.addEventListener('click', () => {
   console.log("Eisme aqui");
@@ -75,6 +58,8 @@ pedir.addEventListener('click', () => {
 
   meuLanche.classList.remove('section-none');
   sectionPedidos.classList.add('section-none');
+
+  title.innerText = "Novo Pedido";
 })
 
 pedido.addEventListener('click', () => {
@@ -83,4 +68,78 @@ pedido.addEventListener('click', () => {
 
   sectionPedidos.classList.remove('section-none');
   meuLanche.classList.add('section-none');
+
+  title.innerText = "Pedidos";
 })
+
+// Envia dados para API
+function fazerRequisicao(url, data) {
+  let loading = document.querySelector("#loading");
+  show(loading);
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      setTimeout(() => {
+        hide(loading);
+      }, 5000);
+      
+      fechaPopup();      
+    } else {
+      throw new Error('Erro na requisição');
+    }
+  })
+  .catch(error => {
+
+    // Reenvia dados para a API
+    setTimeout(() => {
+      fazerRequisicao(data);
+    }, 5000);
+  });
+}
+
+// Fecha Popup
+function fechaPopup() {
+  setTimeout(() => {
+    window.close();
+  }, 500);
+}
+
+// SUMIR
+function hide(id) {
+  id.classList.add("hide")
+}
+
+// APARECER
+function show(id) {
+  id.classList.remove("hide")
+}
+
+let close = document.querySelector("#close");
+close.addEventListener("click", () => {
+  fechaPopup();
+});
+
+// Get the dropdown button and content
+let dropdownBtn = document.querySelector(".dropbtn");
+let dropdownContent = document.querySelector(".dropdown-content");
+
+// Toggle the dropdown content when the button is clicked
+dropdownBtn.addEventListener("click", function() {
+  dropdownContent.classList.toggle("show");
+});
+
+// Close the dropdown when clicking outside of it
+window.addEventListener("click", function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    if (dropdownContent.classList.contains("show")) {
+      dropdownContent.classList.remove("show");
+    }
+  }
+});
